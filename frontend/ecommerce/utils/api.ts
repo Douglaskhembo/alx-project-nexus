9a6484cbeab62575920ea86975cfdee14f341b85
khembo <khembo.douglas.juma@gmail.com>
2025-08-12 01:20:53 +0300

    refresh token url

--- frontend/ecommerce/utils/api.ts
+++ frontend/ecommerce/utils/api.ts
@@ -64,7 +64,7 @@ api.interceptors.response.use(
         }
 
         // Request new access token
-        const res = await axios.post("http://127.0.0.1:8000/api/users/refresh/", {
+        const res = await axios.post(`${process.env.NEXT_PUBLIC_API_URL}/api/users/refresh/`, {
           refresh: refreshToken,
         });
 
